f8e576e1a9dce8c8631e01a03ae43658860e5d80
Michael Simonsen <mikesmejl@gmail.com>
2014-09-18 22:41:25 +0200

    First commit

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,5 @@
 exports = typeof window !== "undefined" && window !== null ? window : global;
-
+//TEst
 exports.Game = function() {
   var players          = new Array();
   var places           = new Array(6);
